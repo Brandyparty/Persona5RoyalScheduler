1b56212f051bf992543cbf94697aa4702be9378d
Andreas Zeh <andi616@t-online.de>
2025-11-09 14:45:49 +0100

    started with calendar

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,47 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import calendar from '../res/p5rcalendar.json'
+
+  console.log(calendar);
+
+  // The actual info this app produces
+  let InfoPerDay = [];
+
+  let weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let stocks = calendar.startingStock;
+
+  let lotteryDay = weekdays[0];
+  let drinkDay = weekdays[0];
+
+  let currentWeekdayIndex = calendar.IndexPreviousDay;
+
+  for (let dayIndex = 0; dayIndex < calendar.days.length; dayIndex++) {
+    let today = calendar.days[dayIndex];
+    currentWeekdayIndex = (currentWeekdayIndex + 1) % weekdays.length;
+    let weekday = weekdays[currentWeekdayIndex];
+
+    let infoForToday = [
+      'Info for ' + today.month + '/' + today.day
+    ];
+
+    if (!('dayUnavaible' in today)) {
+      if (weekday === lotteryDay) {
+        // Reminder for lottery
+        infoForToday.push("Lottery!");
+      }
+      if (weekday === drinkDay) {
+        // Reminder for drink
+        infoForToday.push("Get Sunday drink!");
+      }
+    }
+
+    InfoPerDay.push(infoForToday.join('\n'));
+
+  }
+
+  console.log(InfoPerDay);
+
+
+</script>
 
 <template>
   <h1>You did it!</h1>
